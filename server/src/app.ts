4a028a8f31be870f6273f08a315b7d46cec3e377
Atilola Victor <atilolavictor4562@gmail.com>
2026-08-26 08:50:44 -0700

    refactor: update server application setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,23 @@
 
 import express, { type Application } from 'express'
+import skillRouter from './module/skills/skills.route.ts'
+import { errorMiddleware } from './middleware/error.middleware.ts'
+import companyRouter from './module/company/company.routes.ts'
+import jobRouter from './module/jobs/jobs.routes.ts'
+import careerRouter from './module/career/carreer.route.ts'
+import searchRouter from './module/search/search.route.ts'
 
 const app: Application = express()
 
 app.use(express.json())
 
+app.use('/api/skills', skillRouter)
+app.use('/api/companies', companyRouter)
+app.use('/api/jobs', jobRouter)
+app.use('/api/career', careerRouter)
+app.use('/api/search', searchRouter)
+
+app.use(errorMiddleware)
 
 export default app
+
